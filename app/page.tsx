c15e6ae82c4048e60862638fe7748d08af77d7a5
Fatoye Oluwaseun <fatoyeoluwaseun@gmail.com>
2024-03-01 14:27:53 +0100

    Dashboard page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="min-h-screen">
+      <div className="hero-image"></div>
+
+      <div className="relative max-w-lg p-8 bg-white mx-auto text-center mt-32">
+        Welcome to myApp
+      </div>
+    </main>
+  );
 }
